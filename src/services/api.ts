8803bdb899b43a13dadbb52f5cc5038a76a4d643
Rodolfo Mariano <rodolfo.mariano@outlook.com>
2021-04-24 20:52:30 -0300

    Add readme

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.16:3333'
+  baseURL: 'http://192.168.0.20:3333'
   // baseURL: 'http://192.168.1.112:3333'
+  // baseURL: `${process.env.LOCAL_IP}:33333`
 })
 
 export default api
